7812cbc4cb81c63431369e7c6a4329b59a292fd5
julerrie <julerrie@bu.edu>
2016-04-04 23:14:02 -0400

    some changes
    some changes

--- index.php
+++ index.php
@@ -55,5 +55,50 @@
 	</div>
 	</div>
     <?php include("include/body_bottom.php"); ?>
+<<<<<<< Updated upstream
 </body>
+=======
+    	<div class="services">
+		<div class="container">
+			<div class="
+			">
+				<div class="service secvice-top-grid-1">
+					<img src="/img/doctor-2.jpg" alt="Doc">
+					<h3>Dr. Smith, Cardiologist</h3>
+					<p><!--I am a doctor working in cardiology area for several years. Calculating the end-diastolic and end-systolic volumes of the left ventricle of the heart from cardiac MRI has been a big problem for us since one of my colleagues recommend me Heart2Heart website in which can help us solving this problem and save a lot of time. Further more, this website provides an area to help patients online. And it is my pleasure to answer patients’ questions in H2H. Highly recommend.-->
+					I've been working in cardiology for many years. Calculating the end-diastolic and end-systolic volumes of the left ventricle of the heart from a cardiac MRI has always been a tedious job for cardiologists. One of my colleagues referred me to <em>Heart2Heart</em>, a website that can help us solve this problem and save a lot of time. It also provides an area for patients to see their information and contact their doctors. Answering patients' questions in H2H has been a pleasurable experience. Highly recommend.&nbsp;&nbsp;<a class="btn btn-default">Upload your MRIs &raquo;</a></p>
+					<!--
+<div class="icon1">
+						<i class="icon1"></i>
+					</div>
+-->
+				</div>
+				<div class="service secvice-top-grid-2 service-alternate">
+					<img src="/img/patient.jpg" alt="Patient">
+					<h3>George Papadopoulos, diagnosed with heart disease 1 month ago</h3>
+					<p><!--Several weeks ago I felt my heart painful. So even I have a leg problem and I had to go to the nearest hospital to take a picture which the local doctor calls it MRI. But I can get more information only if I go to the downtown hospital which is so far away for the disable like me. Fortunately, I search it on the internet and find Heart2Heart website. In the website I can upload my MRI in my home and the professional doctors will give me advices. Thank god this new website does help me and I am very grateful.-->
+					Several weeks ago I felt pain in my chest. Even with my leg problem, I had to go all the way to the nearest hospital to get pictures taken of my heart, which the local doctor called MRI. However, I can get more information only if I go to the downtown hospital which is so far away for the disabled like me. Fortunately, I searched on the internet and found the <em>Heart2Heart</em> website. On this site I can upload my MRI from my own home and  medical professionals will give me advice. Thank god for this website, I am very grateful.&nbsp;&nbsp;<a class="btn btn-default">View your MRI results &raquo;</a>
+					</p>
+					<!--
+<div class="icon1">
+						<i class="icon2"></i>
+					</div>
+-->
+				</div>
+				<div class="service secvice-top-grid-3">
+					<img src="/img/classroom.jpg" alt="Doc">
+					<h3>Jane Doe, statistics student at Boston University </h3>
+					<p><!--I am a statistic student in Boston University and my professor Osama giving me a project to make a survey report on the relationship between left ventricle heart volume and the heart disease problem. Osama is a good professor and every student loves him so much. But, oh my gosh, this topic is so medical and should I go to every hospital in Boston to ask them for the data? When I had packed all my package and be ready to start my “hospital trip”, I receive a advertisement email from the Heart2Heart website and surprisingly find that they get the data I need! So it saves me a lot of time and is very convenient to classify the data I need. I have finished my project and get an A from Osama so I want to show my thanks to H2H again. Useful website!-->
+					I am a statistics student at Boston University and my professor, Osama, has given me a project where I have to create a report on the relationship between left ventricle heart volume and heart disease. Osama is a good professor and every student loves him so much. But, oh my gosh, this topic is so medical and do I really have to go to every hospital in Boston to ask them for heart disease data? When I had packed all of my things and was ready to start my "hospital trip", I received an advertisement email from the <em>Heart2Heart</em> website and surprisingly found that they have all the data I need! <em>Heart2Heart</em> saved me a lot of time and is very convenient for classifying the data I need. I have finished my project and got an A from Prof. Osama so I want to show my thanks to <em>H2H</em> again. Useful website!&nbsp;&nbsp;<a class="btn btn-default">Download our public data &raquo;</a>
+					</p>
+					<!--
+<div class="icon1">
+						<i class="icon3"></i>
+					</div>
+-->
+				</div>
+				<div class="clearfix"></div>
+			</div>
+  </body>
+>>>>>>> Stashed changes
 </html>
