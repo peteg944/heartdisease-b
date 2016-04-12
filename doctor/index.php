601598827d974212d811543aa4077eaa53d91dab
Peter Goulakos <goulakos@bu.edu>
2016-04-11 21:13:15 -0400

    css style change, add upload.php

--- doctor/index.php
+++ doctor/index.php
@@ -182,8 +182,7 @@ HTML;
 								<div class="row"><!-- upload file -->
 									<div class="col-xs-12 col-s-6 col-md-6">
 										<h4>Upload a DICOM file</h4>
-										<form action="upload.php" class="dropzone" id="imageUpload">
-											<span class="dz-message">Drop DICOM file here!</span>
+										<form action="upload.php" enctype="multipart/form-data" class="dropzone" id="zipfileUpload">
 										</form>
 									</div>
 								</div>
@@ -202,6 +201,29 @@ HTML;
     	$(document).on('click', '#tab-patients .list-group-item', function(e) {
     		location.href = $(this).attr('href');
     	});
+    	
+    	// Dropzone
+    	Dropzone.options.zipfileUpload = {
+	        paramName: "zipfile",
+	        uploadMultiple: false,
+	        acceptedFiles: "application/zip",
+	        init: function() {
+				this.on("success", function(e, response) {
+					// Hide the image uploader and show the progress thing
+					zipfileUpload.style.display = "none";
+					progressMessage.style.display = "inline";
+					loadImage(response);
+				});
+				
+				this.on("dragover", function(e) {
+					zipfileUpload.style.backgroundColor = "#fee";
+				});
+				
+				this.on("dragleave", function(e) {
+					zipfileUpload.style.backgroundColor = "#fff";
+				})
+			}
+		};
     </script>
   </body>
 </html>
